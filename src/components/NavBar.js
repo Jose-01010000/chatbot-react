0bad00f1e12e0a16d992ed08b167706400f01a91
Jose-01010000 <josepuello112000@gmail.com>
2023-03-11 08:49:42 -0500

    Moduclo ChatBot listo ✅

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+export const NavBar = () => {
+  return (
+    <nav>
+      <div class="navbar-container">
+        <div class="logo">
+          <a href="#">ChatBot Entrevista de Trabajo</a>
+        </div>
+        <div class="navbar-links">
+          <a href="#">Inicio</a>
+          <a href="#">Acerca de</a>
+          <a href="#">Servicios</a>
+          <a href="#">Contacto</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
